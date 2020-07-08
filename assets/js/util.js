e0d76b30df2f981642f6f5b7ec33ff571920a4ee
Harold Blankenship <harold.blankenship@owasp.com>
2020-07-08 09:18:29 -0500

    remove browser check

--- assets/js/util.js
+++ assets/js/util.js
@@ -52,11 +52,11 @@ $(function() {
 
 // browser version checker and notifier
 // source: https://browser-update.org
-var $buoop = {required:{e:-1,f:-3,o:-3,s:-1,c:-3},insecure:true,reminder:0,api:2019.07 }; 
-function $buo_f(){ 
- var e = document.createElement("script"); 
- e.src = "//browser-update.org/update.min.js"; 
- document.body.appendChild(e);
-};
-try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-catch(e){window.attachEvent("onload", $buo_f)}
+// var $buoop = {required:{e:-1,f:-3,o:-3,s:-1,c:-3},insecure:true,reminder:0,api:2019.07 }; 
+// function $buo_f(){ 
+//  var e = document.createElement("script"); 
+//  e.src = "//browser-update.org/update.min.js"; 
+//  document.body.appendChild(e);
+// };
+// try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+// catch(e){window.attachEvent("onload", $buo_f)}
